4f9d81a229812d771bf43850bfdd929148ef5a8c
Dan1el730 <99631797+Dan1el730@users.noreply.github.com>
2024-11-19 10:32:25 +0800

    Delete src/model/utils/FILE_PATHS.java

--- src/model/utils/FILE_PATHS.java
+++
@@ -1,9 +0,0 @@
-package model.utils;
-
-public interface FILE_PATHS {
-    public static final String NAME_PATH = "assets/name_list.txt";
-    public static final String GAMEBOARD_PATH = "assets/gameboard_list.txt";
-    public static final String PROPERTY_PATH = "assets/property_list.txt";
-    public static final String MAPPING_PATH = "assets/square_mapping.txt";
-    public static final String SAVE_PATH = "savefiles";
-}
